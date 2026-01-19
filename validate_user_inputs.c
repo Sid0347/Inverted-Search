abfe87dde218b37fb51afe086dda9aef8aaa978f
Sid0347 <siddharthgaikwad633@gmail.com>
2026-01-19 18:54:17 +0530

    Add create_file_list function to manage valid file linked list

--- validate_user_inputs.c
+++ validate_user_inputs.c
@@ -90,3 +90,39 @@ int is_duplicate(valid_file_list **head2, char *file_name)
     return SUCCESS;
 }
 
+/***************************************************************************************************
+ * Title       : Create File List
+ * Description : This function creates a new node with the given file name and appends it
+ *               to the end of the valid file linked list. If the list is empty, the new node
+ *               becomes the head of the list.
+ * Prototype   : int create_file_list(valid_file_list **head, char *file_name);
+ * Input       : head      : Double pointer to the head of the valid file linked list.
+ *               file_name: Name of the file to be added to the list.
+ * Output      : Status (SUCCESS on successful insertion / FAILURE on memory allocation failure)
+ ***************************************************************************************************/
+int create_file_list(valid_file_list **head, char *file_name)
+{
+    valid_file_list *new = malloc(sizeof(valid_file_list));
+    if (!new)
+    {
+        perror("Allocation : \n");
+        return FAILURE;
+    }
+
+    strcpy(new->files, file_name);
+    new->link = NULL;
+    /* Check if list is empty */
+    if (*head == NULL)
+    {
+        *head = new;
+        return SUCCESS;
+    }
+    /* Check if list is not empty */
+    valid_file_list *temp = *head;
+    while (temp->link != NULL)
+    {
+        temp = temp->link;
+    }
+    temp->link = new;
+    return SUCCESS;
+}
