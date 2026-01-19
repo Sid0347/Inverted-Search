440265dd8945d2fceee88f8ce12016632701407c
Sid0347 <siddharthgaikwad633@gmail.com>
2026-01-19 18:56:08 +0530

    Refactor main function: update input validation and database creation logic

--- main.c
+++ main.c
@@ -10,13 +10,30 @@
 int main(int argc, char *argv[])
 {
 	/* Define the main function here */
+	main_node main_list;
+	sub_node sub_list;
+
+	valid_file_list file_list;
 	valid_file_list *head = NULL;
 
+	/* Create hash table */
+	hash_t hash_arr[SIZE];
+	create_hashtable(hash_arr);
+
+
 	/* Validate CLA */
-	if (validate_user_inputs(&head, argc, argv) == FAILURE)
+	if (process_input_files(&head, argc, argv) == FAILURE)
 	{
 		printf("User inputs validation failed!\n");
 		return 1;
 	}
-}
 
+	/* Create Database */
+	if (create_db(&head, hash_arr, &main_list, &sub_list) == FAILURE)
+	{
+		printf("Create database function failed!\n");
+		return 1;
+	}
+	print_list(head);
+	return 0;
+}
