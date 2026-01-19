d89fd6e3bd66c96f2e11b73be8dade74724f80c9
Sid0347 <siddharthgaikwad633@gmail.com>
2026-01-19 18:54:46 +0530

    Remove create_file_list function and update print_list and create_hashtable functions

--- helper_operations.c
+++ helper_operations.c
@@ -1,46 +1,6 @@
 #include "inverted_search.h"
 
 /***************************************************************************************************
- * Title       : Create File List
- * Description : This function creates a new node with the given file name and appends it
- *               to the end of the valid file linked list. If the list is empty, the new node
- *               becomes the head of the list.
- *
- * Prototype   : int create_file_list(valid_file_list **head, char *file_name);
- *
- * Input       : head      : Double pointer to the head of the valid file linked list.
- *               file_name: Name of the file to be added to the list.
- *
- * Output      : Status (SUCCESS on successful insertion / FAILURE on memory allocation failure)
- ***************************************************************************************************/
-int create_file_list(valid_file_list **head, char *file_name)
-{
-    valid_file_list *new = malloc(sizeof(valid_file_list));
-    if (!new)
-    {
-        perror("Allocation : \n");
-        return FAILURE;
-    }
-
-    strcpy(new->files, file_name);
-    new->link = NULL;
-    /* Check if list is empty */
-    if (*head == NULL)
-    {
-        *head = new;
-        return SUCCESS;
-    }
-    /* Check if list is not empty */
-    valid_file_list *temp = *head;
-    while (temp->link != NULL)
-    {
-        temp = temp->link;
-    }
-    temp->link = new;
-    return SUCCESS;
-}
-
-/***************************************************************************************************
  * Title       : Print File List
  * Description : This function traverses the valid file linked list and prints each file name
  *               in sequence. If the list is empty, it displays an appropriate message.
@@ -66,5 +26,22 @@ void print_list(valid_file_list *head)
 	    }
 
 	    printf("NULL\n");
+    }
+}
+
+/***************************************************************************************************
+ * Title       : Initialize Hash Table
+ * Description : This function initializes all hash table buckets by assigning index values
+ *               and setting the main linked list pointers to NULL.
+ * Prototype   : void init_hashtable(hash_t *hash_arr);
+ * Input       : hash_arr : Pointer to the hash table array.
+ * Output      : Initializes the hash table buckets.
+ ***************************************************************************************************/
+void create_hashtable(hash_t *hash_arr)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        hash_arr[i].index = i;
+        hash_arr[i].main_link = NULL;
     }
 }
